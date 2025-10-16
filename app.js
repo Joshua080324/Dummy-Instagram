03888b0e83e27fdd16a53147ff69df9fbdacadc3
Joshua080324 <darius.josua0309@gmail.com>
2025-10-16 15:46:49 +0700

    tes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 80 || process.env.PORT ;
 const handleError = require('./helpers/handleError');
 const routes = require('./routes');
 const cors = require('cors'); 
