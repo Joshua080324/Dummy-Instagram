dae7053736a349d364cf4b99dcb907ec488bc265
Darius Joshua <darius.josua0309@gmail.com>
2025-10-16 14:05:54 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,13 @@
 module.exports = {
-    apps: [
-        {
-            name: "dummy-instagram-backend",
-            script: "app.js",
-            instances: "max",
-            exec_mode: "cluster",
-            // HANYA AKTIFKAN INI UNTUK DEBUGGING
-            error_file: "logs/app-error.log", // Semua error akan masuk sini
-            out_file: "logs/app-output.log", // Semua output standar akan masuk sini
-            // log_date_format: "YYYY-MM-DD HH:mm:ss", // Opsional, untuk format tanggal di log
-
-            env: {
-                NODE_ENV: "production",
-                PORT: 80,
-                JWT_SECRET: "supersecret",
-                DATABASE_URL: "postgresql://postgres.qwmcsdckdvqjxlstutli:H%2587jz%40TDxZgZzx@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres",
-                DB_SSL: "true",
-                CLOUDINARY_CLOUD_NAME: "dlgeccfib",
-                CLOUDINARY_API_KEY: "493418276358618",
-                CLOUDINARY_API_SECRET: "Jm0DznPX6DFesMHcKI2DPFowSuk",
-                GOOGLE_CLIENT_ID: "426382311425-flmdrhl6n7tmqcamg1lj8c8cmrkqgtpa.apps.googleusercontent.com",
-                GOOGLE_CLIENT_SECRET: "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",
-                GEMINI_API_KEY: "AIzaSyAZj9Jq4lt2usqMCVYrP_rxaX96ZOGl7j4"
-            }
-        }
-    ]
-}
+  apps: [
+    {
+      name: "dummy-instagram-backend",
+      script: "app.js",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+      }
+    }
+  ]
+};
